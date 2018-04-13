56b083d6688640454c0b1860c7484e222b31639d
Quentin Noblet <quentin.noblet.insa@gmail.com>
2018-04-13 09:44:03 +0200

    Initial Commit

---
+++ MecaLaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MecaLaps
+//
+//  Created by Quentin Noblet on 13/04/2018.
+//  Copyright © 2018 Quentin Noblet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
